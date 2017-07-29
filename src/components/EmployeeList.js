9f081298259f4029f6a7607c757df1037373d8a3
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-29 19:12:25 +0500

    Employee List Rendered

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
+import EmployeeListItem from './EmployeeListItem';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeeFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ emp }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(emp);
+  }
+
+  renderRow(employee) {
+    return <EmployeeListItem employee={employee} />;
   }
 
   render() {
     return (
-      <View>
-        <Text>Employee List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
-export default connect(null, { employeeFetch })(EmployeeList);
+const mapStateToProps = state => {
+  const emp = _.map(state.emp, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { emp };
+};
+
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList);
